ec5517715a22ef49b94d786460db1167b115c0e5
RAYEN MHALLA DIT AOUNALLAH <rayan1mhalla@gmail.com>
2026-04-09 16:22:23 +0100

    refactor(form): simplify create flow and client script

--- session-check.php
+++ session-check.php
@@ -3,4 +3,5 @@ declare(strict_types=1);
 
 session_start();
 header('Content-Type: application/json');
-echo json_encode(['loggedIn' => isset($_SESSION['user_id'])]);
+$loggedIn = isset($_SESSION['user_id']);
+echo json_encode(['loggedIn' => $loggedIn]);
