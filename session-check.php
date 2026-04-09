43d8df1b3c54f1a89bd398db075fac27134be441
RAYEN MHALLA DIT AOUNALLAH <rayan1mhalla@gmail.com>
2026-04-09 16:25:30 +0100

    Revert "refactor(form): simplify create flow and client script"
    This reverts commit ec5517715a22ef49b94d786460db1167b115c0e5.

--- session-check.php
+++ session-check.php
@@ -3,5 +3,4 @@ declare(strict_types=1);
 
 session_start();
 header('Content-Type: application/json');
-$loggedIn = isset($_SESSION['user_id']);
-echo json_encode(['loggedIn' => $loggedIn]);
+echo json_encode(['loggedIn' => isset($_SESSION['user_id'])]);
